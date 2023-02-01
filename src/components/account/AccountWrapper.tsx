4da48bc2638ac20270bc4e3953a4110d57336855
Rajiv Titus <75184965+rajivtitus@users.noreply.github.com>
2023-01-31 16:58:46 -0800

    added icons and splash screen

--- src/components/account/AccountWrapper.tsx
+++ src/components/account/AccountWrapper.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, ImageBackground, Image, View } from "react-native";
 import { useTheme } from "react-native-paper";
 
 import { Theme } from "../../ts/types/theme";
-import Loading from "../Loading";
+import FadeInView from "../animations/FadeInView";
 import Spacer from "../Spacer";
 import Typography from "../Typography";
 
@@ -27,18 +27,18 @@ const AccountWrapper = ({ children }: Props) => {
         style={styles.background}
         onLoad={() => setIsImageLoaded(true)}
       >
+        <View style={styles.overlay} />
         {isImageLoaded ? (
-          <>
-            <View style={styles.overlay} />
+          <FadeInView duration={500}>
             <Spacer position="bottom" size="xl">
               <Typography variant="title" style={styles.title}>
                 Delish
               </Typography>
             </Spacer>
             <View style={styles.formContainer}>{children}</View>
-          </>
+          </FadeInView>
         ) : (
-          <Loading />
+          <></>
         )}
       </ImageBackground>
     </View>
@@ -49,6 +49,7 @@ const makeStyles = (theme: Theme) =>
   StyleSheet.create({
     container: {
       flex: 1,
+      backgroundColor: theme.colors.bg.primary,
     },
     background: {
       flex: 1,
@@ -64,6 +65,7 @@ const makeStyles = (theme: Theme) =>
     title: {
       fontSize: 42,
       color: theme.colors.primary,
+      textAlign: "center",
     },
     formContainer: {
       maxWidth: 325,
