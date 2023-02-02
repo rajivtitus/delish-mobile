519115b6ce143267d15d0f6de2952c5ad5949c2c
Rajiv Titus <75184965+rajivtitus@users.noreply.github.com>
2023-02-02 01:29:26 -0800

    removed semicolon from api url

--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -2,4 +2,6 @@ import { FIREBASE_LOCAL_URL, FIREBASE_LIVE_URL } from "@env";
 
 const isDevelopment = process.env.NODE_ENV === "development";
 
-export const host = isDevelopment ? FIREBASE_LOCAL_URL : FIREBASE_LIVE_URL;
+export const host = isDevelopment
+  ? `${FIREBASE_LOCAL_URL}`
+  : `${FIREBASE_LIVE_URL}`;
