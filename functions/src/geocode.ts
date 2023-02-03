23e23c9694eff09a3455e6fa9e5562516199d540
Rajiv Titus <75184965+rajivtitus@users.noreply.github.com>
2023-02-02 16:12:59 -0800

    completed places and geocoding integration

--- functions/src/geocode.ts
+++ functions/src/geocode.ts
@@ -1,14 +1,29 @@
-import { Request, Response } from "firebase-functions/v1";
+import { Request, Response } from "firebase-functions";
+import { Client } from "@googlemaps/google-maps-services-js";
 import * as url from "url";
 
-import { mockLocation } from "./mocks/mockLocation";
-
-export const geocodeRequest = (request: Request, response: Response) => {
+export const geocodeRequest = (
+  request: Request,
+  response: Response,
+  client: Client
+) => {
   const { city } = url.parse(request.url, true).query;
-  const locationFound = mockLocation[city as keyof typeof mockLocation];
-  if (locationFound) {
-    response.json(locationFound);
-  } else {
-    response.send("No location");
+
+  if (!city) {
+    response.status(400).json({ message: "No city specified" });
+    return;
   }
+
+  client
+    .geocode({
+      params: {
+        key: `${process.env.GOOGLE_API_KEY}`,
+        address: city as string,
+      },
+    })
+    .then((res) => response.json(res.data))
+    .catch((err) => {
+      response.status(400);
+      return response.json(err);
+    });
 };
--- functions/src/places.ts
+++ functions/src/places.ts
@@ -1,15 +1,31 @@
 import { Request, Response } from "firebase-functions/v1";
+import { Client } from "@googlemaps/google-maps-services-js";
 import * as url from "url";
 
-import { mockRestaurants } from "./mocks/mockRestaurants";
-
-export const placesRequest = (request: Request, response: Response) => {
+export const placesRequest = (
+  request: Request,
+  response: Response,
+  client: Client
+) => {
   const { location } = url.parse(request.url, true).query;
-  const restaurants = mockRestaurants[location as keyof typeof mockRestaurants];
 
-  if (restaurants) {
-    response.json(restaurants);
-  } else {
-    response.json([]);
+  if (!location) {
+    response.status(400).json({ message: "No location specified" });
+    return;
   }
+
+  client
+    .placesNearby({
+      params: {
+        key: `${process.env.GOOGLE_API_KEY}`,
+        location: location as string,
+        radius: 1500,
+        type: "restaurant",
+      },
+    })
+    .then((res) => response.json(res.data))
+    .catch((err) => {
+      response.status(400);
+      return response.json(err);
+    });
 };
